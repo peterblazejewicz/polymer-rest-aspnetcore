108fb80bcde7254271b34b98c163174392530077
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-10 00:35:09 +0200

    White space + using cleanup

--- src/WebApp/Startup.cs
+++ src/WebApp/Startup.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
